7980d6751b7aa29be7259f4b13acb90054c625c6
Satyabrata Sahoo <satyabrat.me@gmail.com>
2024-05-14 18:49:44 +0530

    feat: setup fonts

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:quantwealth/ui/auth/pages/auth_page.dart';
+import 'package:quantwealth/ui/home/ui/pages/test_page.dart';
 import 'package:web3modal_flutter/web3modal_flutter.dart';
 
 class QuantApp extends StatelessWidget {
@@ -8,15 +7,15 @@ class QuantApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final baseTheme =
+        ThemeData(useMaterial3: true, fontFamily: 'GalanoGrotesque');
+
     return Web3ModalTheme(
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         themeMode: ThemeMode.dark,
-        theme: ThemeData(
-          useMaterial3: true,
-          fontFamily: GoogleFonts.rubik().fontFamily,
-        ),
-        home: AuthPage(),
+        theme: baseTheme,
+        home: TestPage(),
       ),
     );
   }
--- lib/injectable.config.dart
+++ lib/injectable.config.dart
@@ -9,16 +9,18 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:dio/dio.dart' as _i3;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i5;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:logger/logger.dart' as _i4;
 
-import 'di.dart' as _i10;
-import 'ui/auth/cubit/auth_cubit.dart' as _i5;
-import 'ui/balance/cubit/balance_cubit.dart' as _i9;
-import 'ui/balance/infrastructure/repository/balance_repository.dart' as _i7;
-import 'ui/savings/cubit/savings_cubit.dart' as _i8;
-import 'ui/savings/infrastructure/repository/savings_repository.dart' as _i6;
+import 'di.dart' as _i12;
+import 'ui/auth/cubit/auth_cubit.dart' as _i6;
+import 'ui/auth/infrastructure/datasources/auth_local_datasource.dart' as _i7;
+import 'ui/balance/cubit/balance_cubit.dart' as _i11;
+import 'ui/balance/infrastructure/repository/balance_repository.dart' as _i8;
+import 'ui/savings/cubit/savings_cubit.dart' as _i10;
+import 'ui/savings/infrastructure/repository/savings_repository.dart' as _i9;
 
 extension GetItInjectableX on _i1.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -34,21 +36,24 @@ extension GetItInjectableX on _i1.GetIt {
     final diModule = _$DiModule();
     gh.singleton<_i3.Dio>(() => diModule.dio);
     gh.singleton<_i4.Logger>(() => diModule.logger);
-    gh.lazySingleton<_i5.AuthCubit>(() => _i5.AuthCubit());
-    gh.lazySingleton<_i6.SavingsRepository>(() => _i6.SavingsRepository(
+    gh.singleton<_i5.FlutterSecureStorage>(() => diModule.storage);
+    gh.lazySingleton<_i6.AuthCubit>(() => _i6.AuthCubit());
+    gh.singleton<_i7.AuthLocalDatasource>(
+        () => _i7.AuthLocalDatasource(gh<_i5.FlutterSecureStorage>()));
+    gh.lazySingleton<_i8.BalanceRepository>(() => _i8.BalanceRepository(
           dio: gh<_i3.Dio>(),
           logger: gh<_i4.Logger>(),
         ));
-    gh.lazySingleton<_i7.BalanceRepository>(() => _i7.BalanceRepository(
+    gh.lazySingleton<_i9.SavingsRepository>(() => _i9.SavingsRepository(
           dio: gh<_i3.Dio>(),
           logger: gh<_i4.Logger>(),
         ));
-    gh.lazySingleton<_i8.SavingsCubit>(
-        () => _i8.SavingsCubit(savingsRepository: gh<_i6.SavingsRepository>()));
-    gh.lazySingleton<_i9.BalanceCubit>(
-        () => _i9.BalanceCubit(balanceRepository: gh<_i7.BalanceRepository>()));
+    gh.lazySingleton<_i10.SavingsCubit>(() =>
+        _i10.SavingsCubit(savingsRepository: gh<_i9.SavingsRepository>()));
+    gh.lazySingleton<_i11.BalanceCubit>(() =>
+        _i11.BalanceCubit(balanceRepository: gh<_i8.BalanceRepository>()));
     return this;
   }
 }
 
-class _$DiModule extends _i10.DiModule {}
+class _$DiModule extends _i12.DiModule {}
