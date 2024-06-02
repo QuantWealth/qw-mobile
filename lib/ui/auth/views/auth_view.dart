7f6bec8bff7f19c849ec48c14fa713821f2aabe6
Yash-Garg <me@yashgarg.dev>
2024-06-02 15:55:13 +0530

    feat: update icon

--- lib/ui/auth/views/auth_view.dart
+++ lib/ui/auth/views/auth_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:quantwealth/app/theme/icons.dart';
 import 'package:quantwealth/app/theme/theme.dart';
 import 'package:quantwealth/ui/auth/cubit/auth_cubit.dart';
@@ -42,128 +43,141 @@ class _AuthViewState extends State<AuthView> {
       bgImage: 'assets/images/qw_login_bg.png',
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Text(
-              'Let\'s setup your account',
-              style: TextStyle(
-                fontSize: 18,
-                color: white,
-                fontFamily: 'GalanoGrotesque',
-              ),
+            SizedBox(
+              height: 80.0,
+              width: 80.0,
+              child: SvgPicture.asset('assets/icons/logo.svg'),
             ),
-            SizedBox(height: 40),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                IconOutlinedTextButton(
-                  title: 'MetaMask',
-                  assetIcon: SvgIcons.metamask,
-                  onPressed: () => widget.onWalletConnect(),
-                ),
-                IconOutlinedTextButton(
-                  title: 'Coinbase',
-                  assetIcon: SvgIcons.coinbase,
-                  onPressed: () => widget.onWalletConnect(),
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
+            Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                IconOutlinedTextButton(
-                  title: 'WalletConnect',
-                  assetIcon: SvgIcons.walletConnect,
-                  onPressed: () => widget.onWalletConnect(),
+                Text(
+                  'Let\'s setup your account',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: white,
+                    fontFamily: 'GalanoGrotesque',
+                  ),
                 ),
-              ],
-            ),
-            SizedBox(height: 80),
-            Text(
-              'or continue with socials',
-              style: TextStyle(
-                fontSize: 18,
-                color: white,
-                fontFamily: 'GalanoGrotesque',
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                vertical: 20,
-                horizontal: 30,
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: emailController,
-                      decoration: InputDecoration(
-                        contentPadding: EdgeInsets.symmetric(
-                          horizontal: 24,
-                          vertical: 20,
-                        ),
-                        hintText: 'Enter your email...',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: BorderSide(color: Color(0xFFE2E8F0)),
-                        ),
-                        hintStyle: TextStyle(
-                          color: white.withOpacity(0.4),
-                          fontWeight: FontWeight.normal,
-                          fontSize: 14,
-                          fontFamily: 'GalanoGrotesque',
+                SizedBox(height: 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    IconOutlinedTextButton(
+                      title: 'MetaMask',
+                      assetIcon: SvgIcons.metamask,
+                      onPressed: () => widget.onWalletConnect(),
+                    ),
+                    IconOutlinedTextButton(
+                      title: 'Coinbase',
+                      assetIcon: SvgIcons.coinbase,
+                      onPressed: () => widget.onWalletConnect(),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconOutlinedTextButton(
+                      title: 'WalletConnect',
+                      assetIcon: SvgIcons.walletConnect,
+                      onPressed: () => widget.onWalletConnect(),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 80),
+                Text(
+                  'or continue with socials',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: white,
+                    fontFamily: 'GalanoGrotesque',
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 30,
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(
+                              horizontal: 24,
+                              vertical: 20,
+                            ),
+                            hintText: 'Enter your email...',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: BorderSide(color: Color(0xFFE2E8F0)),
+                            ),
+                            hintStyle: TextStyle(
+                              color: white.withOpacity(0.4),
+                              fontWeight: FontWeight.normal,
+                              fontSize: 14,
+                              fontFamily: 'GalanoGrotesque',
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                  SizedBox(width: 20),
-                  SizedBox(
-                    height: 68,
-                    width: 54,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        elevation: .0,
-                        padding: EdgeInsets.zero,
-                        foregroundColor: Colors.black,
-                        backgroundColor: Color(0xFFF1F5F9),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
+                      SizedBox(width: 20),
+                      SizedBox(
+                        height: 68,
+                        width: 54,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            elevation: .0,
+                            padding: EdgeInsets.zero,
+                            foregroundColor: Colors.black,
+                            backgroundColor: Color(0xFFF1F5F9),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          child: Icon(Icons.arrow_forward_outlined),
                         ),
                       ),
-                      child: Icon(Icons.arrow_forward_outlined),
-                    ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgOutlinedButton(
-                  icon: SvgIcons.google,
-                  padding: EdgeInsets.symmetric(vertical: 22),
-                  onPressed: () => widget.onSocialAuth(SocialAuthType.google),
                 ),
-                SizedBox(width: 9),
-                IconOutlinedButton(
-                  icon: Icons.apple,
-                  color: Colors.white,
-                  onPressed: () => widget.onSocialAuth(SocialAuthType.apple),
-                ),
-                SizedBox(width: 9),
-                SvgOutlinedButton(
-                  icon: SvgIcons.email,
-                  padding: EdgeInsets.symmetric(vertical: 22),
-                  onPressed: () => widget.onSocialAuth(SocialAuthType.email),
-                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgOutlinedButton(
+                      icon: SvgIcons.google,
+                      padding: EdgeInsets.symmetric(vertical: 22),
+                      onPressed: () =>
+                          widget.onSocialAuth(SocialAuthType.google),
+                    ),
+                    SizedBox(width: 9),
+                    IconOutlinedButton(
+                      icon: Icons.apple,
+                      color: Colors.white,
+                      onPressed: () =>
+                          widget.onSocialAuth(SocialAuthType.apple),
+                    ),
+                    SizedBox(width: 9),
+                    SvgOutlinedButton(
+                      icon: SvgIcons.email,
+                      padding: EdgeInsets.symmetric(vertical: 22),
+                      onPressed: () =>
+                          widget.onSocialAuth(SocialAuthType.email),
+                    ),
+                  ],
+                )
               ],
-            )
+            ),
           ],
         ),
       ),
