b8dbfc96a14352218140b640df2c841aa945fe3f
Yash-Garg <me@yashgarg.dev>
2024-06-15 19:30:19 +0530

    misc fixes

---
+++ lib/ui/home/ui/pages/strategy_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StrategyPage extends StatefulWidget {
+  const StrategyPage({super.key});
+
+  @override
+  State<StrategyPage> createState() => _StrategyPageState();
+}
+
+class _StrategyPageState extends State<StrategyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
